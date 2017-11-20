d2d6fc1b3fb906b2f2844b5f7f6c841b5a873009
Axel Berneron <berneron.axel@gmail.com>
2017-11-20 10:38:29 -0500

    Add trains to main

--- application/src/main/application/ControleFerroviaire.java
+++ application/src/main/application/ControleFerroviaire.java
@@ -2,22 +2,28 @@ package main.application;
 
 import main.convois.ConvoiTrainA;
 import main.convois.ConvoiTrainB;
+import main.convois.ConvoiTrainC;
 import main.stations.StationAB;
 import main.stations.StationABC;
+import main.stations.StationBC;
 
 public class ControleFerroviaire {
     public static void main(String [] args) {
 
         int nbTrainA = 3;
+        int nbTrainB = 3;
+        int nbTrainC = 3;
 
         StationAB stationAB = new StationAB();
+        StationBC stationBC = new StationBC();
         StationABC stationABC = new StationABC();
 
         Thread convoiTrainA = new Thread(new ConvoiTrainA(nbTrainA, stationAB, stationABC));
+        Thread convoiTrainB = new Thread(new ConvoiTrainB(nbTrainB, stationAB, stationBC, stationABC));
+        Thread convoiTrainC = new Thread(new ConvoiTrainC(nbTrainC, stationBC, stationABC));
+
         convoiTrainA.start();
-
-        //Thread convoiTrainB = new Thread(new ConvoiTrainB(stationAB));
-
         //convoiTrainB.start();
+        //convoiTrainC.start();
     }
 }
